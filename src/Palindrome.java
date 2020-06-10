b947d30ec7ac3d88e2a55a9a1b4bff4a2f4b9906
Billson <Billson@hcbxwy.com>
2020-06-10 12:53:00 +0800

    新增回文数

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+/**
+ * 回文数
+ *
+ * @author Billson
+ * @since 2020/6/10 12:50
+ */
+public class Palindrome {
+
+    /**
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     * 
+     * @param x 整数
+     * @return boolean 
+     * @author Billson
+     * @since 2020/6/10 12:52
+     */
+    public static boolean isPalindrome(int x) {
+        return false;
+    }
+}
