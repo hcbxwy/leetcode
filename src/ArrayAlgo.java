d72e940641b66e1ab0eb4d6daaa7a9c9566ea61e
Billson <billson@hcbxwy.com>
2020-07-24 07:33:01 +0800

    临时提交

---
+++ src/ArrayAlgo.java
@@ -0,0 +1,35 @@
+/**
+ * 数组模块的算法
+ *
+ * @author Billson
+ * @date 2020/7/24 07:07
+ */
+public class ArrayAlgo {
+
+    /**
+     * 寻找数组的中心索引
+     * 中心索引定义：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+     * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+     *
+     * @param nums 整型数组，长度范围为 [0, 10000]，任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数
+     * @return int
+     * @author Billson
+     * @date 2020/7/24 7:08 上午
+     */
+    public int pivotIndex(int[] nums) {
+        int length = nums.length;
+        if (length == 0) {
+            return -1;
+        }
+        // 当数组长度为1，左右侧元素的和都是0，所以数组的元素就是中心索引
+        if (length == 1) {
+            return 0;
+        }
+        for (int i = 0; i < length; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+            // TODO 待实现
+        }
+        return -1;
+    }
+}
